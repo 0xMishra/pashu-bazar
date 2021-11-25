5f615810b567fb7b8c146581925fae3aecf5f9f5
kundan mishra <mishrakundan073@gmail.com>
2021-11-25 10:37:19 +0530

    added loader component

---
+++ src/components/loader.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../static/loader.css";
+const Loader = () => {
+  return (
+    <div class="overlay">
+      <div class="loader"></div>
+    </div>
+  );
+};
+
+export default Loader;
